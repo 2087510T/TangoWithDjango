653721545221bad0387c7d00f226029ebf73008e
Laurynas <2087510T@student.gla.ac.uk>
2015-03-07 00:01:45 +0000

    CHAPTER 15-COMPLETED

--- rango/views.py
+++ rango/views.py
@@ -9,6 +9,7 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from datetime import datetime
+from rango.bing_search import run_query
 
 
 def add_category(request):
@@ -140,3 +141,15 @@ def category(request, category_name_slug):
 @login_required
 def restricted(request):
     return render(request, 'rango/restricted.html',{})
+def search(request):
+
+    result_list = []
+
+    if request.method == 'POST':
+        query = request.POST['query'].strip()
+
+        if query:
+            # Run our Bing function to get the results list!
+            result_list = run_query(query)
+
+    return render(request, 'rango/search.html', {'result_list': result_list})
